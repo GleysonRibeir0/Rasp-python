e7658ec2f8f0679b02870ebd8f516f2cc6ecfaa2
gleyson <gleyson.pr@gmail.com>
2025-08-06 20:49:41 +0200

    commit inicial

---
+++ saudacao.py
@@ -0,0 +1,10 @@
+# Programa simples: saudação personalizada
+
+# Pede o nome do usuário
+nome = input("Qual é o seu nome? ")
+
+# Pede a idade do usuário
+idade = input("Quantos anos você tem? ")
+
+# Exibe a mensagem personalizada
+print("Olá, " + nome + "! Você tem " + idade + " anos.")
