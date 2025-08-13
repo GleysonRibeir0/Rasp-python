2df15db1d23f8c80511e2eaf6cd67ee8fd81aa0e
gleyson <gleyson.pr@gmail.com>
2025-08-13 21:35:46 +0200

    first IF

---
+++ #3_2_double_dice, used IF.py
@@ -0,0 +1,15 @@
+#3_2_double_dice, used IF
+import random
+for x in range(1, 11):
+    throw_1=random.randint(1, 6)
+throw_2=random.randint(1, 6)
+total = throw_1 + throw_2
+print(total)
+if total == 7:
+      print('seven throw !')
+if total == 11:
+      print('eleven throw!')
+if throw_1 == throw_2:
+     print('double thrown!')
+
+    
